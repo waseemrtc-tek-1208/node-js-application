92346c0214bb2a7b5656bafb624a1512d9a91b9a
khanwaseem19 <waseem.khan@rtctek.com>
2026-07-23 18:38:43 +0530

    Added Issue

--- test/index.test.js
+++ test/index.test.js
@@ -7,3 +7,13 @@ function run() {
 }
 
 run();
+
+const assert = require('assert');
+const { getGreeting } = require('../src/utils');
+
+function run() {
+  assert.strictEqual(getGreeting(), 'Hello from node-js-application!');
+  console.log('All tests passed.');
+}
+
+run();
