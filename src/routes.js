b637b7b5ad1962dd093015ad30ddf1d2d4a64f84
khanwaseem19 <waseem.khan@rtctek.com>
2026-07-23 12:27:58 +0530

    commit

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
+const url = require('url');
 const { getGreeting } = require('./utils');
+const { handleUserRequest } = require('./userRoutes');
 
 function handleRequest(req, res) {
-  if (req.url === '/health') {
+  const parsed = url.parse(req.url, true);
+
+  if (parsed.pathname === '/health') {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ status: 'ok' }));
+    return;
+  }
+
+  if (handleUserRequest(req, res, parsed.query)) {
     return;
   }
 
