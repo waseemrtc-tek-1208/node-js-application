27dbbaf4fbc120e8f5bdc18ffa1b4dfe55c2c1e4
khanwaseem19 <waseem.khan@rtctek.com>
2026-07-23 13:40:25 +0530

    adding some changes

--- test/index.test.js
+++ test/index.test.js
@@ -7,3 +7,23 @@ function run() {
 }
 
 run();
+
+const assert = require('assert');
+const { getGreeting } = require('../src/utils');
+
+function run() {
+  assert.strictEqual(getGreeting(), 'Hello from node-js-applications-1!');
+  console.log('All tests passed.');
+}
+
+run();
+
+const assert = require('assert');
+const { getGreeting } = require('../src/utils');
+
+function run() {
+  assert.strictEqual(getGreeting(), 'Hello from node-js-applications-1!');
+  console.log('All tests passed.');
+}
+
+run();
